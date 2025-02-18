f81ca07e67b168e8041ee971b3ccbff8d59769f2
misaka20002 <40714502+misaka20002@users.noreply.github.com>
2025-02-18 19:43:29 +0800

    update

--- Manage/Main.sh
+++ Manage/Main.sh
@@ -286,7 +286,7 @@ if [ "${new_version}" != "${old_version}" ];then
     fi
 fi
 }
-old_version="1.1.8"
+old_version="1.1.9"
 if ping -c 1 gitee.com > /dev/null 2>&1
 then
   VersionURL="https://gitee.com/Misaka21011/Yunzai-Bot-Shell/raw/master/version"
